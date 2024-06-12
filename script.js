3a542b567895311d9fb2d5021ab261c359e7bf9f
Human-Crow <reddit2611@gmail.com>
2024-06-12 22:47:41 +0200

    score calculations

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.getElementById("calc_but").addEventListener("click", function () {
-    var resource_1 = parseFloat(document.getElementById("resource_1").value);
-    var resource_2 = parseFloat(document.getElementById("resource_2").value);
-    var result = resource_1 + resource_2;
-    document.getElementById("result").textContent = "Result: " + result;
-});
+const resources = [730,579,712,599,725,323];
+const et_ratio = [7242, 5028, 7932, 5315, 6954, 3520];
+var score = 100000;
+
+for (let i = 0; i < et_ratio.length; i++) {
+    score = Math.min(score, resources[i] * 30 / et_ratio[i]);
+}
+
+console.log(score);
