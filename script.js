6b7676ea6fad7cfcfdef63227a9d0f3bf32ac203
Human-Crow <reddit2611@gmail.com>
2024-06-13 12:44:55 +0200

    g

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    import GLPK from 'glpk.js';
+    import { GLPK } from 'glpk.js';
     document.getElementById("calc_button").addEventListener("click", function () {
         const glpk = GLPK();
 
